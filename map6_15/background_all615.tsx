db91cb2a895cffa9fedc760f93508efffc5d849e
renjiedai <2312617455@qq.com>
2021-06-15 23:19:11 +0800

    更新地图，用三个瓦片地图组合成一个大地图

---
+++ map6_15/background_all615.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="background_all615" tilewidth="32" tileheight="32" tilecount="22500" columns="150">
+ <image source="background_all615.png" width="4800" height="4800"/>
+</tileset>
