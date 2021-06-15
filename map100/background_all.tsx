1a9ad5bf5ade6ebbd362b498c5f9067fb8395e8c
renjiedai <2312617455@qq.com>
2021-06-15 15:09:22 +0800

    增加一张100*100的32*32的部分地图资源

---
+++ map100/background_all.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="background_all" tilewidth="32" tileheight="32" tilecount="22500" columns="150">
+ <image source="background_all.png" width="4800" height="4800"/>
+</tileset>
