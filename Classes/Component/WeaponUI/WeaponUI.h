80f747eebf060f5c6de368298c8f9fccdc7161ca
Ken <1610348804@qq.com>
2021-06-16 21:47:50 +0800

    +UI显示正在使用的武器

--- Classes/Component/WeaponUI/WeaponUI.h
+++ Classes/Component/WeaponUI/WeaponUI.h
@@ -10,11 +10,6 @@
 #include "Character/Player.h"
 
 
-/*
-???б┬????UI
-	@?????иж
-	*/
-
 
 class WeaponUI : public cocos2d::Node
 {
@@ -32,11 +27,11 @@ protected:
 	int bulletInMagazine_ = 0;
 	int bulletInStock_ = 0;
 
+	cocos2d::Sprite* weaponIcon_;
+
 	cocos2d::Label* bulletInfo_;
 	cocos2d::Label* bulletStockInfo;
-	
 };
 
 
 #endif 
-#pragma once
